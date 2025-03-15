dee912c80e5af21f6d8f2f98a3517a3e69cd2625
Jer <alphastrata@gmail.com>
2025-03-15 23:42:32 +1100

    type checking ok

--- src/gpu_image.rs
+++ src/gpu_image.rs
@@ -1,11 +1,4 @@
-//! The GPU versions of Image and ImageBuffer
-//!
-/*
-
-NOTE: to developers this is where most of the boilerplate can be found
-https://github.com/gfx-rs/wgpu/blob/trunk/examples/features/src/storage_texture/mod.rs
-
-*/
+//! The GPU version of a `sciimg::Image`
 #![allow(unused_imports, dead_code)]
 use crate::{
     enums, image::Image, max, min, path, Dn, DnVec, Mask, MaskVec, MaskedDnVec, MinMax, VecMath,
@@ -13,16 +6,20 @@ use crate::{
 
 use bytemuck::{Pod, Zeroable};
 
-use encase::{internal::WriteInto, ArrayLength, ShaderSize, ShaderType, StorageBuffer};
+use encase::{
+    internal::{ReadFrom, WriteInto},
+    ArrayLength, ShaderSize, ShaderType, StorageBuffer,
+};
 use glam::{Vec3, Vec3A, Vec3Swizzles, Vec4, Vec4Swizzles};
 use thiserror;
+use wgpu::Features;
 
 pub type RGB = Vec3;
 pub type RGBA = Vec4;
 
 // A simple image raster buffer.
 #[derive(ShaderType)]
-pub struct GpuImage<C: ShaderSize + WriteInto> {
+pub struct GpuImage<C: ShaderSize + WriteInto + ReadFrom> {
     pub width: u32,
     pub height: u32,
 
@@ -34,7 +31,7 @@ pub struct GpuImage<C: ShaderSize + WriteInto> {
     #[size(runtime)]
     pub data: Vec<C>,
 }
-impl<C: ShaderSize + WriteInto> GpuImage<C> {
+impl<C: ShaderSize + WriteInto + ReadFrom> GpuImage<C> {
     pub fn new(width: u32, height: u32, data: Vec<C>) -> Self {
         Self {
             width,
@@ -48,10 +45,13 @@ impl<C: ShaderSize + WriteInto> GpuImage<C> {
         buffer.write(self)?;
         Ok(buffer.into_inner())
     }
+    pub fn dimensions(&self) -> (u32, u32) {
+        (self.width, self.height)
+    }
 }
 
 impl GpuImage<Vec3> {
-    pub fn from_sciimg(img: &Image) -> Self {
+    pub fn from_sciimg_rgb(img: &Image) -> Self {
         let size = img.width * img.height;
         let mut data = Vec::with_capacity(size);
         for i in 0..size {
@@ -69,13 +69,10 @@ impl GpuImage<Vec3> {
             data,
         }
     }
-    pub fn dimensions(&self) -> (u32, u32) {
-        (self.width, self.height)
-    }
 }
 
 impl GpuImage<Vec4> {
-    pub fn from_sciimg(img: &Image) -> Self {
+    pub fn from_sciimg_with_alpha(img: &Image) -> Self {
         let size = img.width * img.height;
         let mut data = Vec::with_capacity(size);
         for i in 0..img.width * img.height {
@@ -104,29 +101,37 @@ impl GpuImage<Vec4> {
     }
 }
 
-/*
-
-On supporting enums::ImageMode
-https://gpuweb.github.io/gpuweb/#texture-formats
-    pub enum GpuImageMode {
-        U8BIT,
-        U12BIT,
-        U16BIT,
+trait Empty {
+    fn empty() -> Self;
+}
+impl Empty for GpuImage<Vec3> {
+    fn empty() -> Self {
+        Self {
+            width: 0,
+            height: 0,
+            length: ArrayLength,
+            data: Vec::new(),
+        }
     }
+}
+impl Empty for GpuImage<Vec4> {
+    fn empty() -> Self {
+        Self {
+            width: 0,
+            height: 0,
+            length: ArrayLength,
+            data: Vec::new(),
+        }
+    }
+}
 
-
-    There is no 12BIT support, so you can have U8 or U16
-
-    TODO: GpuTextureFormat to match the wgpu spec our ImageBuffer into -> That.
-*/
-
-// /// A `gpu` wrapper, holding all the wgpu goodies we need to get stuff done
-// // NOTE: You should implement things ON this.
+/// A `gpu` wrapper, holding all the wgpu goodies we need to get stuff done
+// NOTE: You should implement things ON this.
 pub struct GpuContext {
-    instance: wgpu::Instance,
-    adapter: wgpu::Adapter,
-    device: wgpu::Device,
-    queue: wgpu::Queue,
+    pub instance: wgpu::Instance,
+    pub adapter: wgpu::Adapter,
+    pub device: wgpu::Device,
+    pub queue: wgpu::Queue,
 }
 
 impl GpuContext {
@@ -138,15 +143,20 @@ impl GpuContext {
             .unwrap();
 
         let (device, queue) = adapter
-            .request_device(
-                &wgpu::DeviceDescriptor {
-                    label: None,
-                    required_features: todo!(),
-                    required_limits: todo!(),
-                    memory_hints: todo!(),
+            .request_device(&wgpu::DeviceDescriptor {
+                label: Some("SciImg GPU Device"),
+                // These features are required to use `binding_array` in your wgsl.
+                // Without them your shader may fail to compile.
+                required_features: Features::empty(),
+                // Features::BUFFER_BINDING_ARRAY
+                // | Features::STORAGE_RESOURCE_BINDING_ARRAY
+                // | Features::SAMPLED_TEXTURE_AND_STORAGE_BUFFER_ARRAY_NON_UNIFORM_INDEXING,
+                memory_hints: wgpu::MemoryHints::Performance,
+                required_limits: wgpu::Limits {
+                    ..Default::default()
                 },
-                None,
-            )
+                ..Default::default()
+            })
             .await
             .unwrap();
 
@@ -158,23 +168,18 @@ impl GpuContext {
         }
     }
 
-    /// Load the `image` into a GPU Uniform Buffer
-    /// htod (host to device)
-    pub fn load_image_as_uniform<C: ShaderSize + WriteInto>(
+    pub fn load_image_as_storage<C: ShaderSize + WriteInto + ReadFrom>(
         &self,
         image: &GpuImage<C>,
     ) -> (wgpu::Buffer, wgpu::BindGroupLayout, wgpu::BindGroup) {
         let buffer_size = (std::mem::size_of::<C>() * image.data.len()) as wgpu::BufferAddress;
-
         let buffer_desc = wgpu::BufferDescriptor {
-            label: Some("GpuImage Uniform Buffer"),
+            label: Some("GpuImage Storage Buffer"),
             size: buffer_size,
-            usage: wgpu::BufferUsages::UNIFORM | wgpu::BufferUsages::COPY_DST,
+            usage: wgpu::BufferUsages::STORAGE | wgpu::BufferUsages::COPY_DST,
             mapped_at_creation: false,
         };
-
         let buffer = self.device.create_buffer(&buffer_desc);
-
         self.queue.write_buffer(
             &buffer,
             0,
@@ -182,33 +187,165 @@ impl GpuContext {
                 .as_wgsl_bytes()
                 .expect("Unable to write your GpuImage to GPU buffer."),
         );
-        // Create bind group layout
+
         let bind_group_layout =
             self.device
                 .create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
                     entries: &[wgpu::BindGroupLayoutEntry {
                         binding: 0,
-                        visibility: wgpu::ShaderStages::FRAGMENT,
+                        visibility: wgpu::ShaderStages::COMPUTE,
                         ty: wgpu::BindingType::Buffer {
-                            ty: wgpu::BufferBindingType::Uniform,
+                            ty: wgpu::BufferBindingType::Storage { read_only: true },
                             has_dynamic_offset: false,
                             min_binding_size: None,
                         },
                         count: None,
                     }],
-                    label: Some("SciImg Bind Group Layout"),
+                    label: Some("SciImg Storage Bind Group Layout"),
                 });
 
-        // Create bind group
         let bind_group = self.device.create_bind_group(&wgpu::BindGroupDescriptor {
             layout: &bind_group_layout,
             entries: &[wgpu::BindGroupEntry {
                 binding: 0,
                 resource: buffer.as_entire_binding(),
             }],
-            label: Some("SciImg Bind Group"),
+            label: Some("SciImg Storage Bind Group"),
         });
 
         (buffer, bind_group_layout, bind_group)
+    }
+
+    pub fn create_compute_pipeline(
+        &self,
+        layout: &wgpu::PipelineLayout,
+        shader_module: &wgpu::ShaderModule,
+        entry_point: &str,
+    ) -> wgpu::ComputePipeline {
+        self.device
+            .create_compute_pipeline(&wgpu::ComputePipelineDescriptor {
+                label: Some("Compute Pipeline"),
+                layout: Some(layout),
+                module: shader_module,
+                entry_point: Some(entry_point),
+                compilation_options: wgpu::PipelineCompilationOptions::default(),
+                cache: None,
+            })
+    }
+
+    pub fn run_compute_job(
+        &self,
+        pipeline: &wgpu::ComputePipeline,
+        bind_groups: &[&wgpu::BindGroup],
+        workgroup_count_x: u32,
+        workgroup_count_y: u32,
+        workgroup_count_z: u32,
+    ) {
+        let mut encoder = self
+            .device
+            .create_command_encoder(&wgpu::CommandEncoderDescriptor {
+                label: Some("Compute Encoder"),
+            });
+
+        {
+            let mut compute_pass = encoder.begin_compute_pass(&wgpu::ComputePassDescriptor {
+                label: Some("Compute Pass"),
+                timestamp_writes: None,
+            });
+
+            compute_pass.set_pipeline(pipeline);
+
+            for (idx, bind_group) in bind_groups.iter().enumerate() {
+                compute_pass.set_bind_group(idx as u32, *bind_group, &[]);
+            }
+
+            compute_pass.dispatch_workgroups(
+                workgroup_count_x,
+                workgroup_count_y,
+                workgroup_count_z,
+            );
+        }
+
+        self.queue.submit([encoder.finish()]);
+
+        self.device.poll(wgpu::PollType::Wait).unwrap();
+    }
+}
+
+impl GpuContext {
+    pub fn retrieve_storage_data(
+        &self,
+        output_buffer: &wgpu::Buffer,
+        buffer_size: u64,
+    ) -> GpuImage<Vec3> {
+        // Create a staging buffer for reading back data
+        let staging_buffer = self.device.create_buffer(&wgpu::BufferDescriptor {
+            label: Some("Staging Buffer"),
+            size: buffer_size,
+            usage: wgpu::BufferUsages::MAP_READ | wgpu::BufferUsages::COPY_DST,
+            mapped_at_creation: false,
+        });
+
+        // Copy from output buffer to staging buffer
+        let mut encoder = self
+            .device
+            .create_command_encoder(&wgpu::CommandEncoderDescriptor {
+                label: Some("Copy Encoder"),
+            });
+        encoder.copy_buffer_to_buffer(output_buffer, 0, &staging_buffer, 0, buffer_size);
+        self.queue.submit([encoder.finish()]);
+
+        // Map the staging buffer and read the data
+        let buffer_slice = staging_buffer.slice(..);
+        buffer_slice.map_async(wgpu::MapMode::Read, move |_r| {});
+        self.device.poll(wgpu::PollType::Wait).unwrap();
+
+        // Read the mapped range into a Vec<u8>
+        let mapped_range = buffer_slice.get_mapped_range();
+        let mut byte_data = mapped_range.to_vec();
+
+        // Now we can unmap and drop the mapped range
+        drop(mapped_range);
+        staging_buffer.unmap();
+
+        // Create a mutable StorageBuffer with our copied data
+        let buffer = encase::StorageBuffer::new(&mut byte_data);
+
+        // Create an empty result and read into it
+        let mut result: GpuImage<Vec3> = GpuImage::empty();
+        buffer
+            .read(&mut result)
+            .expect("Failed to read from buffer");
+
+        result
+    }
+}
+
+// Add this implementation to your GpuContext struct
+impl GpuContext {
+    pub fn create_storage_buffer<T: ShaderType + WriteInto>(
+        &self,
+        data: &T,
+        label: Option<&str>,
+        usage: wgpu::BufferUsages,
+    ) -> (wgpu::Buffer, u64) {
+        // Create buffer to hold our serialized data
+        let mut byte_buffer = Vec::new();
+        let mut buffer = encase::StorageBuffer::new(&mut byte_buffer);
+        buffer.write(data).expect("Failed to write to buffer");
+
+        // Create the GPU buffer with appropriate usage flags
+        let gpu_buffer = self.device.create_buffer(&wgpu::BufferDescriptor {
+            label,
+            size: byte_buffer.len() as u64,
+            usage,
+            mapped_at_creation: false,
+        });
+
+        // Write our serialized data to the GPU buffer
+        self.queue.write_buffer(&gpu_buffer, 0, &byte_buffer);
+
+        // Return the buffer and its size
+        (gpu_buffer, byte_buffer.len() as u64)
     }
 }
